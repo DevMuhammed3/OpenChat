941675b46cbf7d4384f5bd250d5dc11461ed2b72
devmooo3 <muhdid82@gmail.com>
2025-11-20 01:24:26 +0200

    Remove tsbuildinfo from repo

--- apps/frontend/next.config.js
+++ apps/frontend/next.config.js
@@ -3,6 +3,10 @@ const nextConfig = {
   turbopack: {
     root: "./../..",
   },
+  experimental: {
+    // This disables the floating DevTools button
+    nextDevPanel: false,
+  },
 };
 
 export default nextConfig;
