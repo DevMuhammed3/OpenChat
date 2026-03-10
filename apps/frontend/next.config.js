9e5c92ed35dadf0c5ea20d3e87ef2ecd6d460e9f
devmooo3 <muhdid82@gmail.com>
2026-03-10 05:39:40 +0200

    edit next config

--- apps/frontend/next.config.js
+++ apps/frontend/next.config.js
@@ -1,17 +1,6 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          {
-            key: "Cross-Origin-Opener-Policy",
-            value: "same-origin-allow-popups",
-          },
-        ],
-      },
-    ];
-  },
-};
+  // config
+}
 
-module.exports = nextConfig;
+export default nextConfig
